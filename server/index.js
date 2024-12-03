b8162b07396b269e3958d422c7db5b3c5289c6c8
Yasir Gaji <hellopleasantcodes@gmail.com>
2024-12-03 15:55:00 +0100

    Chore: setup npm scripts

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const exprees = require('express');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+
+app.listen(port, console.log(`Server running on port ${port}`));
