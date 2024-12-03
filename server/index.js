06e609fcf98822b03cff83ec5db4e353dc5700c9
Yasir Gaji <hellopleasantcodes@gmail.com>
2024-12-03 17:51:30 +0100

    Chore: established a project and client relationship in the schema

--- server/index.js
+++ server/index.js
@@ -6,10 +6,12 @@ const port = process.env.PORT || 5005;
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: process.env.NODE_ENV === 'development',
-}))
-
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development',
+  })
+);
 
 app.listen(port, console.log(`Server running on port ${port}`));
