0671e2a5c0441acb2f54bca7d756b4eee2d71d82
Yasir Gaji <hellopleasantcodes@gmail.com>
2024-12-12 15:39:11 +0100

    Chore: configured the edit project mutation

--- client/src/mutations/projectMutations.js
+++ client/src/mutations/projectMutations.js
@@ -1,8 +1,18 @@
-import { gql } from "@apollo/client";
+import { gql } from '@apollo/client';
 
 const ADD_PROJECT = gql`
-  mutation AddProject($name: String!, $description: String!, $status: ProjectStatus!, $clientId: ID!){
-    addProject(name: $name, description: $description, status: $status, clientId: $clientId){
+  mutation AddProject(
+    $name: String!
+    $description: String!
+    $status: ProjectStatus!
+    $clientId: ID!
+  ) {
+    addProject(
+      name: $name
+      description: $description
+      status: $status
+      clientId: $clientId
+    ) {
       id
       name
       description
@@ -18,11 +28,38 @@ const ADD_PROJECT = gql`
 `;
 
 const DELETE_PROJECT = gql`
-  mutation DeleteProject($id: ID!){
+  mutation DeleteProject($id: ID!) {
     deleteProject(id: $id) {
       id
     }
   }
 `;
 
-export { ADD_PROJECT, DELETE_PROJECT }
+const UPDATE_PROJECT = gql`
+  mutation UpdateProject(
+    $id: ID!
+    $name: String!
+    $description: String!
+    $status: ProjectStatusUpdate!
+  ) {
+    updateProject(
+      id: $id
+      name: $name
+      description: $description
+      status: $status
+    ) {
+      id
+      name
+      description
+      status
+      client {
+        id
+        name
+        email
+        phone
+      }
+    }
+  }
+`;
+
+export { ADD_PROJECT, DELETE_PROJECT, UPDATE_PROJECT };
